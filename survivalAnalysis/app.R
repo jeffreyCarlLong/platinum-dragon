be587029e81473e6612eeaa41a7cb89e8f236d94
Jeffrey C. Long <jeffrey.c.long@gmail.com>
2021-10-29 11:35:47 -0700

    Adding Kaplan Meier Shiny App

---
+++ survivalAnalysis/app.R
@@ -0,0 +1,44 @@
+library(tidyverse)
+library(survival)
+library(survminer)
+library(shiny)
+library(dplyr)
+
+tdata <- read_tsv("tdata.txt")
+studychoices=unique(tdata$Study)
+groupchoices=unique(tdata$Group)
+
+
+ui <- fluidPage(
+  
+  
+  titlePanel("Survival Data"),
+  selectInput(inputId = "studyselector",label="Select a Study:", choices=studychoices),
+  selectInput(inputId = "groupselector",label="Select a Group:", choices=groupchoices),
+  plotOutput("p1")
+  
+)
+
+# Define server logic
+server <- function(input, output) {
+  
+  filter=reactive({
+    filteredData=tdata[tdata$Study==input$studyselector,]
+    filteredData['Group'] = ifelse(filteredData$Group==input$groupselector,
+                                   input$groupselector,
+                                   "Others")
+    return(filteredData)
+  })
+  
+  output$p1=renderPlot({
+    fit=survfit(Surv(Time,Censored)~Group,data=filter())
+    ggsurvplot(fit,data=filter(),pval=TRUE,xlim=c(0,max(filter()$Time)+1),
+               title=paste("Study",input$studyselector, "Survival Plot for Group",input$groupselector),
+               xlab="Time (Days)",
+               ggtheme=theme(plot.title=element_text(hjust=0.5)))
+  })
+  
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
